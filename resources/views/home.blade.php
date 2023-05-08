70d7658512465c8c880a8cd24396245a8706a475
alessandroamormino <amormino.alessandro97@gmail.com>
2023-05-08 15:50:43 +0200

    style: linked css file

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Laravel - Homepage</title>
   {{-- css --}}
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
 
